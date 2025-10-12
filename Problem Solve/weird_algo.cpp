a591cab2d8930db1cd8e453f888975d7949965b6
Brian Louis <brianlouis.salinas116@gmail.com>
2025-10-12 00:27:40 -0600

    COMP PROGRAMMING

---
+++ Problem Solve/weird_algo.cpp
@@ -0,0 +1,45 @@
+
+
+#include <iostream>
+using namespace std;
+#define ll long 
+
+int main(){
+
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    ll n;
+    cout << n;
+
+    cin >> n;
+   
+    cout << n;
+
+    
+    while (n > 1) {
+        if (n % 2 == 0) {
+           n = n / 2;
+        }
+        else {
+            n = (n * 3)+ 1;
+        }
+        cout << " " << n;
+    }
+
+    cout << "\n";
+
+
+
+    
+
+
+
+    
+    return 0;
+
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+
+
+    int age;
+    cout << "What is your age: " << age;
+    cin >> age;
+
+    cout << age;
+
+
+    
+
+    return age;
+
+}
+
+
