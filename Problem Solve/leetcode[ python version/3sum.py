eec6cf10497ad360e3b601e1779d88fbedb8069b
Brian Louis <brianlouis.salinas116@gmail.com>
2026-03-09 23:01:01 -0600

    3sum solution :)

---
+++ Problem Solve/leetcode[ python version/3sum.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Use a pointers, but unlike 2sums, we'd put the pointers inside of the 
+        # iteration because we're 
+        
+        list_num = []
+        nums.sort()
+        for x in range(len(nums)):
+            # Pass if duplicate (DISTINCT TRIPLETS!)
+            if x > 0 and nums[x] == nums[x-1]:
+                continue
+            left = x + 1
+            right = len(nums) - 1
+            while left < right:
+                iteration = nums[x] + nums[left] + nums[right]
+                if iteration == 0:
+                    list_num.append([nums[x], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # skip duplicates, DISTINCT TRIPLETS
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+
+                elif iteration < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return list_num
+# Solution Log O(n^2)
