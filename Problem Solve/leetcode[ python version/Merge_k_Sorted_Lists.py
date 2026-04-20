3abdc7f691087652bbbc09d9358a99739fa6b542
Brian Louis <brianlouis.salinas116@gmail.com>
2026-04-19 19:56:44 -0600

    Another one :)

---
+++ Problem Solve/leetcode[ python version/Merge_k_Sorted_Lists.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # First convert the linked list into a an array
+        # We know we have to return it as sorted 
+        result = []
+
+        #  ALSO! the lists is a outer_list
+
+        for i in lists:
+            current = i
+            while current:
+                result.append(current.val)
+                current = current.next
+        result.sort()
+        # Now we convert it back to a linked List
+        new_head = ListNode(0)
+        curr = new_head
+        for i in result:
+            curr.next = ListNode(i)
+            curr = curr.next
+            
+        return new_head.next
+
+
+
