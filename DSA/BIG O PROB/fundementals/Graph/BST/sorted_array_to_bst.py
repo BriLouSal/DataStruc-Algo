d4232249122cf1d4ff06b902c9b06cf58c3724b2
Brian Louis <brianlouis.salinas116@gmail.com>
2026-06-23 19:07:03 -0600

    - Added the efficent solution

--- DSA/BIG O PROB/fundementals/Graph/BST/sorted_array_to_bst.py
+++ DSA/BIG O PROB/fundementals/Graph/BST/sorted_array_to_bst.py
@@ -17,3 +17,32 @@ class Solution:
         node.left = self.sortedArrayToBST(nums[:middle])
         node.right = self.sortedArrayToBST(nums[middle + 1:])
         return node
+    
+
+
+# Efficent Solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        # So using this idea we have to check if the middle value we can 
+        # do this and then let that be centre of the of the array
+        if not nums:
+            return None
+        
+
+        def arr(l, r):
+            if l > r:
+                return None
+
+            mid = (l+ r) // 2
+            node = TreeNode(nums[mid])
+        # Left side will go with the x > middle 
+            node.left = arr(l, mid - 1)
+            node.right = arr( mid + 1,r)
+            return node
+        return arr(0, len(nums)-1)
